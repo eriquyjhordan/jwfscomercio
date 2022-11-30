9ab2a47df6447057b901213595035c71a57219db
Eriquy Jhordan <eriquy@gmail.com>
2022-11-30 13:14:31 -0300

    feat: create sign button

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import styles from '../styles/Index.module.css';
 import Image from 'next/image';
 import backgroundImage from '../public/images/background.webp';
 import jwfsLogo from '../public/jwfs-logo.png';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Image className={styles.backgroundImage} src={backgroundImage} alt="background image" />
       <h1 className={styles.title}>Os melhores conteúdos <br /> em um só lugar</h1>
       <Image className={styles.logo} src={jwfsLogo} alt="logo" />
+      <Link href='' className={styles.signButton}>Assine agora</Link>
     </div>
   );
 }
