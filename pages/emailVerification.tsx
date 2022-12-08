fa464cb6583b9f6bae607f18b8e4d2fdc4b93787
Eriquy Jhordan <eriquy@gmail.com>
2022-12-08 20:37:20 -0300

    fix: unnecessary text

--- pages/emailVerification.tsx
+++ pages/emailVerification.tsx
@@ -30,7 +30,6 @@ export default function EmailVerification() {
         <div className={styles.illustration}>
           <Image src={mailVerificationIllustration.src} alt="Email verification illustration" width={236} height={164} />
           <p className={styles.warninText} style={{ marginTop: '8.6rem' }}>Por segurança enviamos um link de verificação para seu e-mail.</p>
-          <p className={styles.warninText} style={{ marginBottom: '3rem' }}>Abra seu e-mail</p>
         </div>
         <Link href="/">
           <Image src={outlookButton} alt="Outlook button" className={styles.imageButton} />
